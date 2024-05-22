35234ea997572309adb6cbb0a4519f144d1ea9c2
Lm-hg <you@example.com>
2024-05-22 12:28:53 +0200

    firt commit

---
+++ cartes/src/App.jsx
@@ -0,0 +1,15 @@
+import Cartes from './component/Cartes'
+
+import './App.css'
+
+function App() {
+
+  return (
+      <Cartes lien='https://th.bing.com/th/id/OIP.OjAmIvE9b0gLSS1pe6nelQHaEK?w=302&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7' nom="Pikachu" cout='4'  explication='
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus blanditiis sed obcaecati, odio adipisci aspernatur reprehenderit voluptatum, quis eveniet libero quasi eius numquam quo fuga possimus cumque ratione incidunt temporibus!'></Cartes>
+
+
+  )
+}
+
+export default App
---
+++ cartes/src/component/Cartes.jsx
@@ -0,0 +1,39 @@
+import Keys from "./keys"
+export default function Cartes(props){
+    const keysword=[
+        {keysword:"Eclair"},
+        {keysword:"Tonnerre"}
+    ]
+    return (
+        <div className="card">
+            <div className="illustration">
+                <div className="image">
+                    <img src={props.lien} alt="" srcset="" />
+                </div>
+                <div className="nom">
+                    <h2>{props.nom}</h2>
+                </div>
+                <div className="cout">
+                    <h3>{props.cout}</h3>
+                </div>
+            </div>
+                <div className="effet">
+                        <div className="keywords">
+
+                    {keysword.map((a)=>(
+
+                            <Keys keys={a.keysword}/>
+                    ))}
+                        </div>
+                <h2>Effet</h2>
+                <p className="explication">
+                    {props.explication}
+                </p>
+                </div>
+            <div className="type">
+                <h3>Type</h3>
+            </div>
+        
+        </div>
+    )
+}
